40d64b55ed4fd9263d375ee51cd7d4e64bec060f
Ciaran Kelly <92984593+CiaranMichaelKelly@users.noreply.github.com>
2021-11-22 14:47:32 +0100

    First drop

---
+++ Profile/homeProfile.ps1
@@ -0,0 +1,18 @@
+﻿# Set Error Text Color
+(Get-Host).PrivateData.ErrorForegroundColor = "green"
+ 
+# Setting directory location
+Set-Location C:\Dev\PowerShell
+
+#Start with a clear screen
+Clear-Host
+
+#import docker module for tab completion
+Import-Module posh-docker
+
+#Welcome Message
+Write-Host "Computer name and user: $env:ComputerName\$env:UserName" -ForegroundColor Cyan
+ 
+#Version information
+Write-Host "Powershell version information:"
+$PSversiontable.PSVersion
