622ee4b95a46eab809a824e2696b55fdd374b467
Ciaran Kelly <92984593+CiaranMichaelKelly@users.noreply.github.com>
2021-11-22 15:53:42 +0100

    removed unwanted lines

--- Profile/homeProfile.ps1
+++ Profile/homeProfile.ps1
@@ -2,13 +2,10 @@
 (Get-Host).PrivateData.ErrorForegroundColor = "green"
  
 # Setting directory location
-Set-Location C:\Dev\PowerShell
+Set-Location C:\Code\PowerShell
 
 #Start with a clear screen
 Clear-Host
-
-#import docker module for tab completion
-Import-Module posh-docker
 
 #Welcome Message
 Write-Host "Computer name and user: $env:ComputerName\$env:UserName" -ForegroundColor Cyan
